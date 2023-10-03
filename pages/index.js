9717b160ce25349f3a90a1ca4086604d7bdd7ca1
Parthmad <100795861+Parthmad@users.noreply.github.com>
2023-10-03 09:59:23 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,24 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+async function query(data) {
+    const response = await fetch(
+        "http://localhost:3000/api/v1/prediction/5209eae0-447d-4e73-83db-64f518b187d9",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }
+    );
+    const result = await response.json();
+    return result;
+}
 
+query({"question": "Hey, how are you?"}).then((response) => {
+    console.log(response);
+});
       <main>
         <Header title="Anrok Test" />
         <p className="description">
