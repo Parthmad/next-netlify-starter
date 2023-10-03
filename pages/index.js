d4eed2446a62285a5a0598bc27e1fa9505a20954
Parthmad <100795861+Parthmad@users.noreply.github.com>
2023-10-03 09:57:50 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,24 +16,7 @@ export default function Home() {
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
-async function query(data) {
-    const response = await fetch(
-        "http://localhost:3000/api/v1/prediction/5209eae0-447d-4e73-83db-64f518b187d9",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        }
-    );
-    const result = await response.json();
-    return result;
-}
 
-query({"question": "Hey, how are you?"}).then((response) => {
-    console.log(response);
-});
       <Footer />
     </div>
   )
