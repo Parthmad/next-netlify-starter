37be840992d0d71414f346fdbdfd438e3962a2f8
Parthmad <100795861+Parthmad@users.noreply.github.com>
2023-10-03 10:00:23 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,27 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-async function query(data) {
-    const response = await fetch(
-        "http://localhost:3000/api/v1/prediction/5209eae0-447d-4e73-83db-64f518b187d9",
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data)
-        }
-    );
-    const result = await response.json();
-    return result;
-}
+        <script type="module">
+    import Chatbot from "https://cdn.jsdelivr.net/npm/flowise-embed/dist/web.js"
+    Chatbot.init({
+        chatflowid: "5209eae0-447d-4e73-83db-64f518b187d9",
+        apiHost: "http://localhost:3000",
+    })
+</script>
 
-query({"question": "Hey, how are you?"}).then((response) => {
-    console.log(response);
-});
       <main>
         <Header title="Anrok Test" />
         <p className="description">
